52835e7475faffbcc6ff16c6576a1a91b504e597
Rawatsandeep001 <sandeeprawat25691@gmail.com>
2026-05-22 10:08:54 +0530

    Refactor ansibleDeploy.groovy to use resource properties
    Refactored ansibleDeploy.groovy to load properties directly from a resource file instead of reading from a local config file. Removed unnecessary stages and streamlined environment variable assignments.

--- vars/ansibleDeploy.groovy
+++ vars/ansibleDeploy.groovy
@@ -1,82 +1,13 @@
-// vars/ansibleDeploy.groovy
+def props = new Properties()
+props.load(new StringReader(libraryResource('config.properties')))
 
-def call() {
+env.SLACK_CHANNEL_NAME = props['SLACK_CHANNEL_NAME']
+env.ENVIRONMENT        = props['ENVIRONMENT']
+env.CODE_BASE_PATH     = props['CODE_BASE_PATH']
+env.ACTION_MESSAGE     = props['ACTION_MESSAGE']
+env.KEEP_APPROVAL_STAGE= props['KEEP_APPROVAL_STAGE']
 
-    node {
-
-        def config = readProperties file: 'resources/config.properties'
-
-        env.SLACK_CHANNEL_NAME = config.SLACK_CHANNEL_NAME
-        env.ENVIRONMENT        = config.ENVIRONMENT
-        env.CODE_BASE_PATH     = config.CODE_BASE_PATH
-        env.ACTION_MESSAGE     = config.ACTION_MESSAGE
-        env.KEEP_APPROVAL_STAGE= config.KEEP_APPROVAL_STAGE
-
-        env.GIT_URL            = config.GIT_URL
-        env.BRANCH             = config.BRANCH
-        env.PLAYBOOK           = config.PLAYBOOK
-        env.INVENTORY          = config.INVENTORY
-
-
-        // -----------------------------
-        // Stage 1: Clone
-        // -----------------------------
-        stage('Clone Code') {
-
-            echo "Cloning repository..."
-
-            git branch: env.BRANCH,
-                url: env.GIT_URL
-
-            echo "Code cloned successfully"
-        }
-
-
-        // -----------------------------
-        // Stage 2: User Approval
-        // -----------------------------
-        if (env.KEEP_APPROVAL_STAGE == "true") {
-
-            stage('User Approval') {
-
-                echo "Waiting for approval..."
-
-                input message: "Approve deployment to ${env.ENVIRONMENT} environment?",
-                      ok: "Approve"
-
-                echo "Approval received"
-            }
-        }
-
-
-        // -----------------------------
-        // Stage 3: Playbook Execution
-        // -----------------------------
-        stage('Ansible Playbook Execution') {
-
-            echo "Running Ansible Playbook..."
-
-            sh """
-            ansible-playbook -i ${env.INVENTORY} ${env.PLAYBOOK}
-            """
-
-            echo "Playbook execution completed"
-        }
-
-
-        // -----------------------------
-        // Stage 4: Notification
-        // -----------------------------
-        stage('Notification') {
-
-            echo "Sending Slack Notification..."
-
-            slackSend(
-                channel: "#${env.SLACK_CHANNEL_NAME}",
-                message: "${env.ACTION_MESSAGE} | Environment: ${env.ENVIRONMENT}"
-            )
-
-            echo "Notification sent"
-        }
-    }
-}
+env.GIT_URL            = props['GIT_URL']
+env.BRANCH             = props['BRANCH']
+env.PLAYBOOK           = props['PLAYBOOK']
+env.INVENTORY          = props['INVENTORY']
